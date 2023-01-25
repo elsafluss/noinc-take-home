b71c66163d23912b7ce1c01a62019fb863b08824
Elsa Fluss <elsafluss@gmail.com>
2023-01-25 14:20:49 -0700

    Display user's interests on home page cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,25 @@
 import React from "react"
 import "./Card.css"
 
-export const Card = () => {
-  return (
-    <>
-      <div className="card-container">
-        <div className="card-title">
-          <p>Card title</p>
+export const Card = ({ interests }) => {
+  if (!interests) {
+    return <h1> you are not interesting, please get a hobby </h1>
+  } else {
+    const interestCard = interests.map((interest, i) => {
+      return (
+        <div className="card-container" key={i}>
+          <div className="card-title">
+            <p>Interest</p>
+          </div>
+          <div className="card-details">
+            <div className="card-name-key">NAME:</div>
+            <div className="card-name-value">{interest.name}</div>
+            <div className="card-type-key">TYPE:</div>
+            <div className="card-type-value">{interest.type}</div>
+          </div>
         </div>
-        <div className="card-details">
-          <div className="card-name-key">NAME:</div>
-          <div className="card-name-value">card details name</div>
-          <div className="card-type-key">TYPE:</div>
-          <div className="card-type-value">card details type</div>
-        </div>
-      </div>
-    </>
-  )
+      )
+    })
+    return <div>{interestCard}</div>
+  }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,31 +4,37 @@ import "./Home.css"
 import { Nav } from "./Nav.js"
 import { Card } from "./Card"
 
-export const Home = (userData) => {
-  console.log(userData)
-  return (
-    <>
-      <Nav></Nav>
-      <div className="home-container">
-        <div className="home-header-title">
-          <p>Welcome {userData.userName}</p>
+export const Home = ({ userData }) => {
+  const username = userData.userData.userData.userName
+  const interests = userData.userData.userData.interests
+  if (username && interests) {
+    console.log(interests)
+    console.log(username)
+    return (
+      <>
+        <Nav></Nav>
+        <div className="home-container">
+          <div className="home-header-title">
+            <p>Welcome, {username}</p>
+          </div>
+          <div className="home-header-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+            euismod bibendum laoreet. Proin gravida dolor sit amet lacus
+            accumsan et viverra justo commodo. Proin sodales pulvinar sic
+            tempor. Sociis natoque penatibus et magnis dis parturient montes,
+            nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra
+            vulputate, felis tellus mollis orci, sed rhoncus pronin sapien nunc
+            accuan eget.
+          </div>
         </div>
-        <div className="home-header-text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-          euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan
-          et viverra justo commodo. Proin sodales pulvinar sic tempor. Sociis
-          natoque penatibus et magnis dis parturient montes, nascetur ridiculus
-          mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus
-          mollis orci, sed rhoncus pronin sapien nunc accuan eget.
+        <div className="home-content">
+          <Card interests={interests}></Card>
         </div>
-      </div>
-      <div className="home-content">
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-      </div>
-    </>
-  )
+      </>
+    )
+  } else {
+    return null
+  }
 }
 
 Home.propTypes = {
